992ccf71dddd1a0127e373045793cae231e82fc5
Paulo Eduardo <paulo.es@live.com>
2019-06-11 23:53:45 -0300

    config mongo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+console.log(process.env.MONGO_URL);
+//mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 app.get("/", (req, res) => {
   return res.send("Hello World");
